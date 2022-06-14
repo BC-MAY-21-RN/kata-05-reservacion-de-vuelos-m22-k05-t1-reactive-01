f682910224b895afc21f29dc2a757fcc4a0655ba
FranciscoSenestrari <fran.senes@gmail.com>
2022-06-14 18:36:36 -0300

    add validation inputs

--- App/src/screen/SignIn/SignIn.js
+++ App/src/screen/SignIn/SignIn.js
@@ -6,6 +6,8 @@ import Input from '../../component/Input/Input';
 import Footer from '../../component/Footer/Footer';
 import {styles} from './style';
 import ChBox from '../../component/CheckBox/ChBox';
+import {useFormik} from 'formik';
+import * as Yup from 'yup';
 
 const SignIn = ({navigation}) => {
   const [state, setstate] = useState({
@@ -23,29 +25,46 @@ const SignIn = ({navigation}) => {
     console.log(state);
   };
 
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    validateOnChange: false,
+    onSubmit: formValue => {
+      console.log('Formulario enviado..');
+      console.log(formValue);
+    },
+  });
+
   return (
     <View style={styles.container}>
       <View style={styles.containerTitle}>
         <Text style={styles.title}>Sing up</Text>
       </View>
       <View style={styles.containerInput}>
+        <Text style={styles.inputError}>{formik.errors.firstName}</Text>
         <Input
           title="First Name"
-          onChangeText={value => handleChangeText('First Name', value)}
           textContentType={'emailAddress'}
           keyboardType="email-address"
+          value={formik.values.firstName}
+          onChangeText={text => formik.setFieldValue('firstName', text)}
         />
+        <Text style={styles.inputError}>{formik.errors.email}</Text>
         <Input
           title="Email*"
           textContentType="emailAddress"
           keyboardType="email-address"
+          value={formik.values.email}
+          onChangeText={text => formik.setFieldValue('email', text)}
         />
+        <Text style={styles.inputError}>{formik.errors.password}</Text>
 
         <Input
           title="Password*"
           textContentType="password"
           secureTextEntry={true}
-          onChangeText={value => handleChangeText('password', value)}
+          value={formik.values.password}
+          onChangeText={text => formik.setFieldValue('password', text)}
         />
       </View>
       <View>
@@ -64,7 +83,7 @@ const SignIn = ({navigation}) => {
         <ChBox label="Subscribe for select product updates" />
       </View>
       <View style={styles.containerButton}>
-        <ButtonForm title="Sing Up" onPress={() => login()} />
+        <ButtonForm title="Sing Up" onPress={formik.handleSubmit} />
         <Text style={styles.textButtonGroup}> or </Text>
         <ButtonGoogle title="Sign Up with Google" />
       </View>
@@ -78,4 +97,19 @@ const SignIn = ({navigation}) => {
   );
 };
 
+function initialValues() {
+  return {
+    firstName: '',
+    email: '',
+    password: '',
+  };
+}
+
+function validationSchema() {
+  return {
+    firstName: Yup.string().required('The user is oblitoried'),
+    email: Yup.string().email().required('Email in use.Use a diferent email'),
+    password: Yup.string().required('Incorrect password'),
+  };
+}
 export default SignIn;
