dfd245d9e2d54b6027011de4fa2eb339de68845a
FranciscoSenestrari <fran.senes@gmail.com>
2022-06-27 15:51:56 -0300

    move content myflight to home
    Co-authored-by: nicolasjrz <nicolasjrz.dev@gmail.com>
    Co-authored-by: ReymartinMontes <reymartinmontes@gmail.com>
    Co-authored-by: ayeza-zamudio3 <ayezahmvc14@gmail.com>
    Co-authored-by: robermancera <robermancera@gmail.com>

--- App/src/hubs/MainStack.js
+++ App/src/hubs/MainStack.js
@@ -4,7 +4,6 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from '../screen/Home/Home';
 import Login from '../screen/Login/Login';
 import SignIn from '../screen/SignIn/SignIn';
-import MyFlight from '../screen/MyFlights/MyFlight';
 export default function mainStack() {
   const Stack = createNativeStackNavigator();
 
@@ -18,7 +17,6 @@ export default function mainStack() {
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="SignIn" component={SignIn} />
-        <Stack.Screen name="MyFlight" component={MyFlight} />
       </Stack.Navigator>
     </NavigationContainer>
   );
