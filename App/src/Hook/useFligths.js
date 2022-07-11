a700801204b06285cc1aaa8c461985b5923dada0
FranciscoSenestrari <fran.senes@gmail.com>
2022-07-11 20:17:30 -0300

    fix refresh flight
    Co-authored-by: Nicolas juarez <nicolasjrz@users.noreply.github.com>
    Co-authored-by: ReymartinMontes <ReymartinMontes@users.noreply.github.com>
    Co-authored-by: ayeza-zamudio3 <ayezahmvc14@gmail.com>

--- App/src/Hook/useFligths.js
+++ App/src/Hook/useFligths.js
@@ -2,7 +2,8 @@ import React, {useEffect, useState} from 'react';
 import {getJourney, getData} from '../database/friestore';
 export default function useFligts() {
   const [fligths, setFlights] = useState([]);
-
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
   useEffect(() => {
     const timer = setTimeout(() => {
       getDataBd();
@@ -11,13 +12,42 @@ export default function useFligts() {
   }, []);
 
   const getDataBd = async () => {
-    const resp = await getData();
-    if (resp.length > 0) {
-      setFlights(resp);
+    setIsLoading(true);
+    try {
+      const resp = await getData();
+      if (resp.length > 0) {
+        setFlights(resp);
+        setIsSuccess(true);
+        setIsLoading(false);
+      } else {
+        alert('no hay mas datos');
+        setIsSuccess(true);
+        setIsLoading(false);
+      }
+    } catch (error) {
+      setIsLoading(false)
+      setIsSuccess(false)
+      console.log(error);
+    }
+  };
+
+  return {fligths, getDataBd, isSuccess, isLoading};
+}
+/*
+  useEffect(() => {
+    const query = getDataBd();
+  }, [fligths]);
+
+  const getDataBd = async () => {
+    const {arrayFlights, usersQuerySnapshot} = await getData();
+    if (arrayFlights.length > 0) {
+      setFlights(arrayFlights);
     } else {
       alert('no hay mas datos');
     }
+    return usersQuerySnapshot;
   };
 
   return {fligths};
 }
+*/
--- App/src/screen/Home/Home.js
+++ App/src/screen/Home/Home.js
@@ -1,5 +1,12 @@
-import React from 'react';
-import {View, Text, StyleSheet, ScrollView, FlatList} from 'react-native';
+import React, {useEffect} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  FlatList,
+  ActivityIndicator,
+} from 'react-native';
 import FlightsCard from '../../component/FlightCard/flightsCard';
 import ButtonAdd from '../../component/Button/ButtonAdd';
 import {styles} from './style.js';
@@ -8,38 +15,48 @@ import {logout} from '../../authentication/loginEmail/AuthProvider';
 import Header from '../../component/Header/Header';
 
 const Home = ({navigation}) => {
-  const {fligths} = useFligts();
+  const {fligths, getDataBd, isLoading, isSuccess} = useFligts();
+
+  useEffect(() => {
+    getDataBd();
+  }, []);
 
   const {origin, destiny, date, passenger} = fligths;
   return (
-    <View>
-      <View style={styles.container}>
-        <Header
-          type={true}
-          onPress={() => {
-            logout(navigation);
-          }}
-        />
-        <FlatList
-          data={fligths}
-          keyExtractor={item => item.id}
-          pagingEnabled={true}
-          renderItem={({item, index}) => (
-            <FlightsCard
-              date={item.date}
-              iataCodeFrom={item.origin.iso_airport}
-              to={item.destiny.country}
-              from={item.origin.country}
-              iataCodeTo={item.destiny.iso_airport}
-              passenger={'passenger: ' + item.passenger}
+    <>
+      {!isLoading && isSuccess ? (
+        <View>
+          <View style={styles.container}>
+            <Header
+              type={true}
+              onPress={() => {
+                logout(navigation);
+              }}
             />
-          )}
-        />
-      </View>
-      <View style={styles.button}>
-        <ButtonAdd onPress={() => navigation.navigate('From')} />
-      </View>
-    </View>
+            <FlatList
+              data={fligths}
+              keyExtractor={item => item.id}
+              pagingEnabled={true}
+              renderItem={({item, index}) => (
+                <FlightsCard
+                  date={item.date}
+                  iataCodeFrom={item.origin.iso_airport}
+                  to={item.destiny.country}
+                  from={item.origin.country}
+                  iataCodeTo={item.destiny.iso_airport}
+                  passenger={'passenger: ' + item.passenger}
+                />
+              )}
+            />
+          </View>
+          <View style={styles.button}>
+            <ButtonAdd onPress={() => navigation.navigate('From')} />
+          </View>
+        </View>
+      ) : (
+        <ActivityIndicator />
+      )}
+    </>
   );
 };
 
